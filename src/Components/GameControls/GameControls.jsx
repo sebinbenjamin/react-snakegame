d62621e8aacc1bdf5b0a6738f1da6ee9fe66483c
Sebin Benjamin <sebinbenjamin@gmail.com>
2019-06-04 10:55:18 +0530

    fixed the pause/resume button

--- src/Components/GameControls/GameControls.jsx
+++ src/Components/GameControls/GameControls.jsx
@@ -2,20 +2,25 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import style from "./GameControls.module.css";
+import playImage from "../../resources/images/play.svg"
+import pauseImage from "../../resources/images/pause.svg"
 
 const GameControls = (props) => {
   const { isPaused, changeGamePlay } = props;
+  const gamePlayButtonStyle = {
+    background: `url(${isPaused ? playImage : pauseImage}) center top/contain no-repeat`
+  }
+
   return (
     <div className={style.GameControls}>
       <div className={style.flexContainer}>
-        <button type="button" className={style.play} onClick={(event) => changeGamePlay(event)} />
-        {isPaused ? '' : 'Game Paused'}
-        <button type="button"> Reset</button>
+        <button type="button" className={style.play} style={gamePlayButtonStyle} onClick={(event) => changeGamePlay(event)} />
+        <span className={style.statusMsg}>{isPaused ? 'Gameplay Paused' : ''}</span>
+        <button type="button" className={style.reset}>Reset</button>
       </div>
     </div>
   )
 }
-
 
 GameControls.propTypes = {
   isPaused: PropTypes.bool.isRequired,
